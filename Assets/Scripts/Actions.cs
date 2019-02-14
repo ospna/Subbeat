738f19461010f5ee047f0a011b8a790662f5627c
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2019-02-14 00:29:07 -0500

    Quick UI Protoype

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Actions : MonoBehaviour
+{
+    public void Game()
+    {
+        SceneManager.LoadScene("Subbeat");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Upgrade()
+    {
+        //SceneManager.LoadScene("Upgrade");
+    }
+
+    public void Music()
+    {
+        SceneManager.LoadScene("Music");
+    }
+
+}
